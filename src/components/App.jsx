0d785f8126f81570d3a2ab06a70ffdcf21e561da
harshshekhawat7773 <harshshekhawat963@gmail.com>
2022-07-06 23:46:30 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import "./style.css";
+import emoji from "./emoji";
+
+function emojiren(contact){
+    return 
+}
+export default function App(props){
+    return <div>
+        <h1 className="heading">Emoji World</h1>
+        <div className="row row_1">
+            <div className="col-3 column">
+               emoji.map(contact)
+            </div>
+            <div className="col-3 column">
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+            </div>
+            <div className="col-3 column">
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+            </div>
+            <div className="col-3 column">
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+            </div>
+            <div className="col-3 column">
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+            </div>
+            <div className="col-3 column">
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+            </div>
+            <div className="col-3 column">
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+            </div>
+            <div className="col-3 column">
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+                lorem30vbvsbvns vscs ofwih harsh vardhan sinfh shekhawat
+            </div>
+
+        </div>
+    </div>
+}
